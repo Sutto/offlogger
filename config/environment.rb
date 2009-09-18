548bc6442b19508aad972c6d8a51d86b8af71b58
Darcy Laycock <sutto@sutto.net>
2009-09-19 00:54:40 +0800

    Added basic authorization stuff

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'authlogic'
   config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
   config.gem 'searchlogic'
   config.gem 'rr'
