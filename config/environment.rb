59dc776f318323242222a5d9dfd26dcf6917531e
Darcy Laycock <sutto@sutto.net>
2009-09-19 00:52:04 +0800

    Added gems to the app

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,12 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
+  config.gem 'searchlogic'
+  config.gem 'rr'
+  config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com', :version => '>= 2.2.3'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
